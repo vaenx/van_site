f107877818fb0404108294ceaed1c86925c3a865
Vanessa <vanessaveigavilela@gmail.com>
2018-07-13 18:03:09 +0100

    LP and about me page done

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(){
 		Typed.new(".entry_typewritter", {
-			strings: ["Hello. I’m Vanessa, a UX Designer from Lisbon", "Last year I've been busy building Brixel with my team", "And researching about design impact in users' choices", "Check my profiles for more info below."],
+			strings: ["Hello, I design for meaningful experiences."],
 			typeSpeed: 40,
-      loop: true,
+      loop: false,
 		});
 	});
